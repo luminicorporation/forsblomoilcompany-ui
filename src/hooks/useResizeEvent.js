d1f0bd01c80713caece170125e28626d4e68ddcd
Pablo Garcia <pgarciacamou@gmail.com>
2019-04-11 12:47:52 -0700

    export throttled resize hook

--- src/hooks/useResizeEvent.js
+++ src/hooks/useResizeEvent.js
@@ -1,4 +1,5 @@
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useMemo } from 'react';
+import throttle from 'lodash.throttle';
 
 export const useResizeEvent = handler => {
   useLayoutEffect(() => {
@@ -6,4 +7,12 @@ export const useResizeEvent = handler => {
     window.addEventListener('resize', handler, { passive: true });
     return () => window.removeEventListener('resize', handler);
   }, []);
+};
+
+export const useThrottledResizeEvent = (handler, delay = 100) => {
+  const throttledHandler = useMemo(() => throttle(handler, delay), [
+    handler,
+    delay,
+  ]);
+  useResizeEvent(throttledHandler);
 };
